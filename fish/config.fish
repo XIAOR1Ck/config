01b838cacd88e7bb09e216f46bdcfa5ce8eb3be8
Suresh Acharya <sureshach.off@gmail.com>
2024-04-06 20:12:06 +0545

    2024/04/06

--- fish/config.fish
+++ fish/config.fish
@@ -17,3 +17,105 @@ set -Ux XMODIFIERS @im=fcitx
 
 ## alises
 alias ls="lsd"
+alias clear="echo -en '\e[H\e[J\e[3J'; seq 1 (tput cols) | sort -R | spark | lolcat"
+
+function fish_greeting
+	banner
+end
+## for extra functionalities use colorscript script by DT for greetings.
+## spark.fish by jorgebucaran ##
+function spark --description Sparklines
+    argparse --ignore-unknown --name=spark v/version h/help m/min= M/max= -- $argv || return
+
+    if set --query _flag_version[1]
+        echo "spark, version 1.1.0"
+    else if set --query _flag_help[1]
+        echo "Usage: spark <numbers ...>"
+        echo "       stdin | spark"
+        echo "Options:"
+        echo "       --min=<number>   Minimum range"
+        echo "       --max=<number>   Maximum range"
+        echo "       -v or --version  Print version"
+        echo "       -h or --help     Print this help message"
+        echo "Examples:"
+        echo "       spark 1 1 2 5 14 42"
+        echo "       seq 64 | sort --random-sort | spark"
+    else if set --query argv[1]
+        printf "%s\n" $argv | spark --min="$_flag_min" --max="$_flag_max"
+    else
+        command awk -v min="$_flag_min" -v max="$_flag_max" '
+            {
+                m = min == "" ? m == "" ? $0 : m > $0 ? $0 : m : min
+                M = max == "" ? M == "" ? $0 : M < $0 ? $0 : M : max
+                nums[NR] = $0
+            }
+            END {
+                n = split("▁ ▂ ▃ ▄ ▅ ▆ ▇ █", sparks, " ") - 1
+                while (++i <= NR) 
+                    printf("%s", sparks[(M == m) ? 3 : sprintf("%.f", (1 + (nums[i] - m) * n / (M - m)))])
+            }
+        ' && echo
+    end
+end
+## spark.fish end ##
+
+## fish_logo.fish ##
+function fish_logo \
+    --description="Fish-shell colorful ASCII-art logo" \
+    --argument-names outer_color medium_color inner_color mouth eye
+
+    # defaults:
+    [ $outer_color  ]; or set outer_color  'red'
+    [ $medium_color ]; or set medium_color 'f70'
+    [ $inner_color  ]; or set inner_color  'yellow'
+    [ $mouth ]; or set mouth '['
+    [ $eye   ]; or set eye   'O'
+
+    set usage 'Usage: fish_logo <outer_color> <medium_color> <inner_color> <mouth> <eye>
+See set_color --help for more on available colors.'
+
+    if contains -- $outer_color '--help' '-h' '-help'
+        echo $usage
+        return 0
+    end
+
+    # shortcuts:
+    set o (set_color $outer_color)
+    set m (set_color $medium_color)
+    set i (set_color $inner_color)
+
+    if test (count $o) != 1; or test (count $m) != 1; or test (count $i) != 1
+        echo 'Invalid color argument'
+        echo $usage
+        return 1
+    end
+
+    echo '                 '$o'___
+  ___======____='$m'-'$i'-'$m'-='$o')
+/T            \_'$i'--='$m'=='$o')
+'$mouth' \ '$m'('$i$eye$m')   '$o'\~    \_'$i'-='$m'='$o')
+ \      / )J'$m'~~    '$o'\\'$i'-='$o')
+  \\\\___/  )JJ'$m'~'$i'~~   '$o'\)
+   \_____/JJJ'$m'~~'$i'~~    '$o'\\
+   '$m'/ '$o'\  '$i', \\'$o'J'$m'~~~'$i'~~     '$m'\\
+  (-'$i'\)'$o'\='$m'|'$i'\\\\\\'$m'~~'$i'~~       '$m'L_'$i'_
+  '$m'('$o'\\'$m'\\)  ('$i'\\'$m'\\\)'$o'_           '$i'\=='$m'__
+   '$o'\V    '$m'\\\\'$o'\) =='$m'=_____   '$i'\\\\\\\\'$m'\\\\
+          '$o'\V)     \_) '$m'\\\\'$i'\\\\JJ\\'$m'J\)
+                      '$o'/'$m'J'$i'\\'$m'J'$o'T\\'$m'JJJ'$o'J)
+                      (J'$m'JJ'$o'| \UUU)
+                       (UU)'(set_color normal)
+end
+## fish_logo.fish end ##
+
+## banner print ##
+function banner
+	set rnum (random)
+	set num (math $rnum % 15)
+	set fname (echo "banner$num.txt")
+	cat ~/.config/scripts/$fname | lolcat
+end
+
+## banner end ##
+#
+
